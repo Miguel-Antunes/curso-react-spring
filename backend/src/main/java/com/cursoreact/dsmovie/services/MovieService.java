879c81738a86bbcb348f64c8ce88419cb891e32b
Miguel-antunes <miguel.antunes.costa10@gmail.com>
2022-05-15 18:55:20 -0300

    Save Score

--- backend/src/main/java/com/cursoreact/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/cursoreact/dsmovie/services/MovieService.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 import com.cursoreact.dsmovie.dto.MovieDTO;
 import com.cursoreact.dsmovie.entities.Movie;
